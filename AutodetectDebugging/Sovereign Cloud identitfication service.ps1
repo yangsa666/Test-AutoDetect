c64f88e8861a4fa333aee1e2befdae929bb1d9f2
Wayne Yang <sayang@microsoft.com>
2020-11-05 23:44:28 +0800

    update script

--- AutodetectDebugging/Sovereign Cloud identitfication service.ps1
+++ AutodetectDebugging/Sovereign Cloud identitfication service.ps1
@@ -1,7 +1,16 @@
 #Sovereign Cloud identitfication services
 $configServiceUrl = "https://officeclient.microsoft.com/config16processed?rs=en-us&build=16.0.7612"
 $headers = @{'Accept' = 'application/json'}
+
+#Call configService to get emailHrd service Url
 $getFederationProviderEndpoint = Invoke-WebRequest -Uri "$($configServiceUrl)&services=GetFederationProvider" -Headers $headers -Method GET
+$obj1 = $getFederationProviderEndpoint.Content | ConvertFrom-Json
+$emailHrdUrl = $obj1.'o:OfficeConfig'.'o:services'.'o:service'.'o:url'
+
+#Call configService to get autoDiscover service Url
 $getAutoDiscoverEndpoint = Invoke-WebRequest -Uri "$($configServiceUrl)&services=ExchangeAutoDiscoverV2Url" -Headers $headers -Method GET
-$xml1 = $getFederationProviderEndpoint.Content | ConvertTo-Xml
-$xml2 = $getAutoDiscoverEndpoint.Content | ConvertTo-Xml
+$obj2 = $getAutoDiscoverEndpoint.Content | ConvertFrom-Json
+$autoDiscoverUrl = $obj2.'o:OfficeConfig'.'o:services'.'o:service'.'o:url'
+
+
+
