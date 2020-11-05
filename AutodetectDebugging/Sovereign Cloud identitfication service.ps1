ae479ed318574fbaaf4ad51175b833e750ca3bc2
Wayne Yang <sayang@microsoft.com>
2020-11-06 00:25:25 +0800

    Updated Script

--- AutodetectDebugging/Sovereign Cloud identitfication service.ps1
+++ AutodetectDebugging/Sovereign Cloud identitfication service.ps1
@@ -1,3 +1,9 @@
+param (
+    [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [String[]]$SMTP
+)
+$SMTPAddress = $SMTP.Split("@") 
+
 #Sovereign Cloud identitfication services
 $configServiceUrl = "https://officeclient.microsoft.com/config16processed?rs=en-us&build=16.0.7612"
 $headers = @{'Accept' = 'application/json'}
@@ -12,5 +18,46 @@ $getAutoDiscoverEndpoint = Invoke-WebRequest -Uri "$($configServiceUrl)&services
 $obj2 = $getAutoDiscoverEndpoint.Content | ConvertFrom-Json
 $autoDiscoverUrl = $obj2.'o:OfficeConfig'.'o:services'.'o:service'.'o:url'
 
+#Call emailHrd service
+$webResponse = Invoke-WebRequest -Uri "$($emailHrdUrl)?domain=$($SMTPAddress[1])" -Headers $headers -Method GET
+$emailHrdResult = $webResponse.Content
+if(!$emailHrdResult -eq "Global") {
+    callAutoDiscover
+}
+else {
+    callAutoDetect
+}
 
+#Call AutoDiscover service
+function callAutoDiscover {
+    param (
+        OptionalParameters
+    )
+    
+}
 
+#Discover AAD Authority
+function FunctionName {
+    param (
+        OptionalParameters
+    )
+    
+}
+
+#Call AutoDetect service
+function callAutoDetect {
+    $autoDetectURL = "https://prod-autodetect.outlookmobile.com/detect?protocols=eas,rest-cloud,imap,pop3,smtp&timeout=13.5&services=office365,outlook,google,icloud,yahoo"
+    $encodedEmailAddress = [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes($SMTP))
+    $authorizationHeader = @{'Authorization' = "Basic " + $encodedEmailAddress}
+    $webResponse1 = Invoke-WebRequest -Uri $autoDetectURL -Headers $authorizationHeader -Method GET
+    $autoDetectResult = $webResponse1.Content | ConvertFrom-Json
+    $requestId = $webResponse1.Headers.'X-Request-Id'
+}
+
+Call OnPrem AutoDiscoverV2
+function callOnPremAutoDiscover {
+    param (
+        OptionalParameters
+    )
+    
+}
